e4625518dc9fdf3dccb1139599a329aee2796667
davidski <davidski@deadheaven.com>
2014-08-09 20:13:35 -0700

    Kitchen support and cleanup

--- Berksfile
+++ Berksfile
@@ -2,3 +2,4 @@ site :opscode
 
 metadata
 cookbook 'elasticsearch', github: 'elasticsearch/cookbook-elasticsearch'
+cookbook 'java'
---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+default['elasticsearch']['path']['data'] = ["/es/data"]
+default['elasticsearch']['path']['logs'] = "/es/logs"
+
+default['elasticsearch']['version'] = '1.2.3'
+default['elasticsearch']['index']['number_of_replicas'] = 0
